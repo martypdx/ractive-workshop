d8a845f252dd2c32b7f2d83f2b0843fffb9215fc
marty <marty.nelson@yahoo.com>
2014-09-20 08:39:44 -0700

    loader examples

--- gobblefile.js
+++ gobblefile.js
@@ -1,17 +1,24 @@
-var gobble = require('gobble'),
-	marked = require('./gobble/gobble-marked')
+var gobble = require('gobble') //,
+// 	marked = require('./gobble/gobble-marked')
 
 
-var assets = gobble('assets')
+// var assets = gobble('assets')
 
-var mdcss = '<link rel="stylesheet" type="text/css" href="../../css/style.css">\n' +
-			'<link rel="stylesheet" type="text/css" href="../../css/tomorrow.css">\n'
+// var mdcss = '<link rel="stylesheet" type="text/css" href="../../css/style.css">\n' +
+// 			'<link rel="stylesheet" type="text/css" href="../../css/tomorrow.css">\n'
 
-	markdown = assets.include('**/*.md')
-			.map(marked)
-			.map(function(code){
-				return mdcss + code
-			}),
-	allelse = assets.exclude('**/*.md')
+// 	markdown = assets.include('**/*.md')
+// 			.map(marked)
+// 			.map(function(code){
+// 				return mdcss + code
+// 			}),
+// 	allelse = assets.exclude('**/*.md')
 
-module.exports = gobble([markdown, allelse])
+// module.exports = gobble([markdown, allelse, components])
+
+var components = gobble( 'assets/G-and-beyond/ractive_components' )
+module.exports = gobble([
+	components.exclude('index.html').map( 'ractive', { type: 'amd' }),
+	components.include('index.html'),
+	gobble('assets/js')
+])
